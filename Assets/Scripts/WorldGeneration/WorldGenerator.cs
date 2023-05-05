d297a3957dd5bfd1efc7894a0b8fce37bf79d192
Egor Ageev <54112318+Lunarett@users.noreply.github.com>
2023-05-05 22:54:43 +0200

    Basic perlin biome generation

--- Assets/Scripts/WorldGeneration/WorldGenerator.cs
+++ Assets/Scripts/WorldGeneration/WorldGenerator.cs
@@ -7,6 +7,11 @@ public class WorldGenerator : MonoBehaviour
     public int size;
     public int regionAmount;
     public int regionColorAmount;
+    public List<Color> myRegionColors = new List<Color>();
+    public float scale = 12;
+    public int octaves = 4;
+    public float persistance = 0.1f;
+    public float lacunarity = 2.2f;
 
     private void Start()
     {
@@ -28,6 +33,11 @@ public class WorldGenerator : MonoBehaviour
             regionColors[i] = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
         }
 
+        for(int i = 0; i < myRegionColors.Count; i++)
+        {
+            regionColors[i] = myRegionColors[i];
+        }
+
         Color[] pixelColors = new Color[size * size];
 
         for(int y = 0; y < size; y++)
@@ -36,7 +46,7 @@ public class WorldGenerator : MonoBehaviour
             {
                 float distance = float.MaxValue;
                 int value = 0;
-                float perlinValue = PerlinNoise.GenerateBiomeNoise(x, y, 12, 4, 0.1f, 2.2f);
+                float perlinValue = PerlinNoise.GenerateBiomeNoise(x, y, scale, octaves, persistance, lacunarity);
 
                 for(int i = 0; i < regionAmount; i++)
                 {
@@ -79,7 +89,26 @@ public class WorldGenerator : MonoBehaviour
             }
         }
 
+        for(int i = 0; i < regionAmount / 2; i++)
+        {
+            for(int amount = 1; amount <= 2; amount++)
+            {
+                for(int y = amount; y < size - amount; y++)
+                {
+                    for(int x = amount; x < size - amount; x++)
+                    {
+                        if(pixelColors[(x + amount) + y * size] != pixelColors[x + y * size] && pixelColors[(x - amount) + y * size] != pixelColors[x + y * size] || 
+                        pixelColors[x + (y + amount) * size] != pixelColors[x + y * size] && pixelColors[x +(y - amount) * size] != pixelColors[x + y * size]) 
+                        {
+                            pixelColors[x + y * size] = pixelColors[(x + amount) + y * size];
+                        }
+                    }
+                }
+            }
+        }
+
         Texture2D myTexture = new Texture2D(size, size);
+        myTexture.filterMode = FilterMode.Point;
         myTexture.SetPixels(pixelColors);
         myTexture.Apply();
 
