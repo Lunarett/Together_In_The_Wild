9278e7e905914fba7533e88ca45cd63f452788cf
Egor Ageev <54112318+Lunarett@users.noreply.github.com>
2023-05-22 19:05:13 +0200

    fixed voronoi regions

--- Assets/Scripts/WorldGeneration/WorldGenerator.cs
+++ Assets/Scripts/WorldGeneration/WorldGenerator.cs
@@ -1,102 +1,131 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    [Header("World Settings")]
-    public int size;
-    public int regionAmount;
-    public int regionColorAmount;
-    public float scale = 12;
-    public int octaves = 4;
-    public float persistance = 0.1f;
-    public float lacunarity = 2.2f;
-    public BiomeSettings biomeSettings;
-    
+    [SerializeField] private int worldSize = 1024;
+    [SerializeField] private int regionAmount = 10;
+    [SerializeField] private BiomeSettings biomeSettings;
     private BiomeData[] biomeData;
+    private Vector2[] points;
+    private Color[] pixelColors;
+    private Texture2D myTexture;
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
 
     private void Start()
     {
         biomeData = biomeSettings.Biomes;
+        points = GenerateRandomPoints();
+        pixelColors = new Color[worldSize * worldSize];
+        myTexture = new Texture2D(worldSize, worldSize);
+        myTexture.filterMode = FilterMode.Point;
         CreateVoronoiDiagram();
+        ApplyTextureToMaterial();
     }
 
-    public void CreateVoronoiDiagram()
+    private void CreateVoronoiDiagram()
+    {
+        int biomeDataLength = biomeData.Length;
+
+        for (int y = 0; y < worldSize; y++)
+        {
+            for (int x = 0; x < worldSize; x++)
+            {
+                int value = FindClosestPointIndex(new Vector2(x, y));
+                int closestRegionIndex = FindClosestRegionIndex(new Vector2(x, y), value);
+                float perlinValue = Perlin.GenerateNoiseMap(x, y, 100, biomeData[value % biomeData.Length].perlinParams);
+                bool useBiomeData = AssignPixelColorWithBiomeData(value, perlinValue, x, y, biomeDataLength);
+
+                if (!useBiomeData)
+                    pixelColors[x + y * worldSize] = GetPixelColorBasedOnPerlinValue(perlinValue, value, closestRegionIndex);
+            }
+        }
+
+        myTexture.SetPixels(pixelColors);
+        myTexture.Apply();
+    }
+
+    private Vector2[] GenerateRandomPoints()
     {
         Vector2[] points = new Vector2[regionAmount];
-        Color[] regionColors = new Color[regionColorAmount];
+        int textureSizeMinusOne = worldSize - 1;
 
         for (int i = 0; i < regionAmount; i++)
         {
-            points[i] = new Vector2(Random.Range(0, size), Random.Range(0, size));
+            points[i] = new Vector2(Random.Range(0, worldSize), Random.Range(0, worldSize));
         }
 
-        Color[] pixelColors = new Color[size * size];
+        return points;
+    }
 
-        for (int y = 0; y < size; y++)
+    private int FindClosestPointIndex(Vector2 position)
+    {
+        int value = 0;
+        float distance = float.MaxValue;
+
+        for (int i = 0; i < regionAmount; i++)
         {
-            for (int x = 0; x < size; x++)
+            float dist = Vector2.SqrMagnitude(position - points[i]);
+            if (dist < distance)
             {
-                float distance = float.MaxValue;
-                int value = 0;
+                distance = dist;
+                value = i;
+            }
+        }
 
-                for (int i = 0; i < regionAmount; i++)
+        return value;
+    }
+
+    private int FindClosestRegionIndex(Vector2 position, int excludeIndex)
+    {
+        int closestRegionIndex = 0;
+        float distanceRegion = float.MaxValue;
+
+        for (int i = 0; i < regionAmount; i++)
+        {
+            if (i != excludeIndex)
+            {
+                float dist = Vector2.SqrMagnitude(position - points[i]);
+                if (dist < distanceRegion)
                 {
-                    if (Vector2.Distance(new Vector2(x, y), points[i]) < distance)
-                    {
-                        distance = Vector2.Distance(new Vector2(x, y), points[i]);
-                        value = i;
-                    }
-                }
-
-                int closestRegionIndex = 0;
-                float distanceRegion = float.MaxValue;
-                for (int i = 0; i < regionAmount; i++)
-                {
-                    if (i != value)
-                    {
-                        if (Vector2.Distance(new Vector2(x, y), points[i]) < distanceRegion)
-                        {
-                            distanceRegion = Vector2.Distance(new Vector2(x, y), points[i]);
-                            closestRegionIndex = i;
-                        }
-                    }
-                }
-
-                float perlinValue = Perlin.GenerateNoiseMap(x, y, 1283, biomeData[value % biomeData.Length].perlinParams);
-                //float perlinValue = Perlin.GenerateNoiseMap(x, y, 123, new PerlinParams(scale, octaves, persistance, lacunarity));
-
-                bool useBiomeData = false;
-                for (int j = 0; j < biomeData[value % biomeData.Length].colorLayers.Length; j++)
-                {
-                    if (perlinValue >= biomeData[value % biomeData.Length].colorLayers[j % biomeData[value % biomeData.Length].colorLayers.Length].range.x &&
-    perlinValue <= biomeData[value % biomeData.Length].colorLayers[j % biomeData[value % biomeData.Length].colorLayers.Length].range.y)
-                    {
-                        pixelColors[x + y * size] = biomeData[value % biomeData.Length].colorLayers[j].layerColor;
-                        useBiomeData = true;
-                        break; // Exit the loop after assigning the color
-                    }
-                }
-
-                if (!useBiomeData)
-                {
-                    if (perlinValue < 0.5f)
-                    {
-                        pixelColors[x + y * size] = regionColors[value % regionColorAmount];
-                    }
-                    else
-                    {
-                        pixelColors[x + y * size] = regionColors[closestRegionIndex % regionColorAmount];
-                    }
+                    distanceRegion = dist;
+                    closestRegionIndex = i;
                 }
             }
         }
 
-        Texture2D myTexture = new Texture2D(size, size);
-        myTexture.filterMode = FilterMode.Point;
-        myTexture.SetPixels(pixelColors);
-        myTexture.Apply();
+        return closestRegionIndex;
+    }
 
-        GetComponent<Renderer>().material.mainTexture = myTexture;
+    private bool AssignPixelColorWithBiomeData(int value, float perlinValue, int x, int y, int biomeDataLength)
+    {
+        var biome = biomeData[value % biomeDataLength];
+
+        float perlinNoise = VoronoiNoiseRegion.PerlinNoise.GenerateBiomeNoise(x, y, biome.perlinParams.scale, biome.perlinParams.octaves, biome.perlinParams.persistence, biome.perlinParams.lacunarity);
+
+        foreach (var colorLayer in biome.colorLayers)
+        {
+            if (perlinNoise >= colorLayer.range.x && perlinNoise <= colorLayer.range.y)
+            {
+                pixelColors[x + y * worldSize] = colorLayer.layerColor;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Color GetPixelColorBasedOnPerlinValue(float perlinValue, int value, int closestRegionIndex)
+    {
+        return perlinValue < 0.5f ? Color.white : Color.black;
+    }
+
+    private void ApplyTextureToMaterial()
+    {
+        material.mainTexture = myTexture;
     }
 }
